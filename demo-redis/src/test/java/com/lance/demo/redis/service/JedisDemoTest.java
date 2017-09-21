b0468813c81b25365230359aa7e65b8e4766c229
袁其亮 <yuanqiliang@51kik.com>
2017-09-21 10:34:17 +0800

    add demo with pub sub

---
+++ demo-redis/src/test/java/com/lance/demo/redis/service/JedisDemoTest.java
@@ -0,0 +1,39 @@
+package com.lance.demo.redis.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JedisDemoTest {
+    JedisDemo jedisDemo = new JedisDemo();
+    @Test
+    public void set() throws Exception {
+    }
+
+    @Test
+    public void set1() throws Exception {
+    }
+
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void hset() throws Exception {
+    }
+
+    @Test
+    public void hset1() throws Exception {
+    }
+
+    @Test
+    public void pub() throws Exception {
+        jedisDemo.pub();
+    }
+
+    @Test
+    public void sub() throws Exception {
+        jedisDemo.sub();
+    }
+
+}
